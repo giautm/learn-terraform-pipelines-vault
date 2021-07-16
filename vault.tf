0d23a7721682ea70cefc8ceb264a5a04e190bf7a
Giau. Tran Minh <giau.tmg@gmail.com>
2021-07-17 04:11:12 +0700

    fix: remove submodule

--- vault.tf
+++ vault.tf
@@ -1,8 +1,12 @@
 resource "helm_release" "vault" {
   count     = data.terraform_remote_state.cluster.outputs.enable_consul_and_vault ? 1 : 0
   name      = "${data.terraform_remote_state.consul.outputs.release_name}-vault"
-  chart     = "${path.module}/vault-helm"
   namespace = data.terraform_remote_state.consul.outputs.namespace
+
+  repository = "https://helm.releases.hashicorp.com"
+  chart      = "vault"
+  version    = "0.13.0"
+
 
   set {
     name  = "server.ha.enabled"
